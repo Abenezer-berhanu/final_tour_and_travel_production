ad96134605280aeceb9b913b8f596b063f7f1bdc
Abenezer <abenezerberhanu40@gmail.com>
2024-05-22 13:54:37 +0300

    MODIFY:hover

--- src/components/uiComponents/NavbarLinks.jsx
+++ src/components/uiComponents/NavbarLinks.jsx
@@ -7,7 +7,7 @@ function NavbarLinks() {
       {navItems.map((data, idx) => (
         <p
           key={idx}
-          className="font-semibold tracking-wide capitalize hover:text-primary"
+          className="font-semibold tracking-wide capitalize hover:text-primary_orange"
         >
           <Link href={data.link}>{data.label}</Link>
         </p>
