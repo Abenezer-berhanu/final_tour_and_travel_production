3cd481fb0d0623be0a0ea2040fe6630474cfd647
Abenezer <abenezerberhanu40@gmail.com>
2024-05-23 09:57:36 +0300

    MODIFY: styling to admin

--- src/app/admin/tours/page.jsx
+++ src/app/admin/tours/page.jsx
@@ -3,7 +3,7 @@ import AdminSmallUsersTable from "@/components/uiComponents/AdminSmallUsersTable
 function page() {
   return (
     <div className="flex flex-col gap-3">
-      <h1 className="text-2xl font-bold">All Users</h1>
+      <h1 className="text-2xl font-bold">All Tours</h1>
       <AdminSmallUsersTable />
     </div>
   );
--- src/components/uiComponents/DashboardLayout.jsx
+++ src/components/uiComponents/DashboardLayout.jsx
@@ -1,12 +1,15 @@
+"use client";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { FaHome, FaUser, FaSubway } from "react-icons/fa";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function DashboardLayout() {
+  const pathname = usePathname();
   return (
     <div className="bg-main h-full bg-primary text-white flex flex-col gap-10 p-3 w-full flex-grow">
-        <h1 className="text-2xl font-bold mx-auto">My Dashboard</h1>
+      <h1 className="text-2xl font-bold mx-auto">My Dashboard</h1>
       <div className="flex flex-col gap-1 items-center justify-center">
         <Avatar className="size-24">
           <AvatarImage src="https://github.com/shadcn.png" />
@@ -19,7 +22,9 @@ function DashboardLayout() {
       <div className="flex flex-col items-start justify-center gap-3">
         <Link
           href={"/admin/dashboard"}
-          className="flex w-full items-center justify-center bg-white text-black rounded-md p-1 cursor-pointer text-primary hover:bg-primary_orange duration-200"
+          className={`flex w-full items-center justify-center ${
+            pathname == "/admin/dashboard" ? "bg-primary_orange" : "bg-white text-primary"
+          } text-black rounded-md p-1 cursor-pointer hover:bg-primary_orange duration-200`}
         >
           <div className="flex gap-3 items-center py-5 min-w-[120px] max-h-[30px]">
             <FaHome size={25} className="" />
@@ -28,16 +33,20 @@ function DashboardLayout() {
         </Link>
         <Link
           href={"/admin/users"}
-          className="flex items-center justify-center w-full bg-white text-black rounded-md p-1 cursor-pointer text-primary hover:bg-primary_orange duration-200"
+          className={`flex w-full items-center justify-center ${
+            pathname == "/admin/users" ? "bg-primary_orange" : "bg-white text-primary"
+          } text-black rounded-md p-1 cursor-pointer hover:bg-primary_orange duration-200`}
         >
           <div className="flex gap-3 items-center py-5 min-w-[120px] max-h-[30px]">
             <FaUser size={25} />
-            <b>User</b>
+            <b>Users</b>
           </div>
         </Link>
         <Link
           href={"/admin/tours"}
-          className="flex items-center justify-center w-full bg-white text-black rounded-md p-1 cursor-pointer text-primary hover:bg-primary_orange duration-200"
+          className={`flex w-full items-center justify-center ${
+            pathname == "/admin/tours" ? "bg-primary_orange" : "bg-white text-primary"
+          } text-black rounded-md p-1 cursor-pointer hover:bg-primary_orange duration-200`}
         >
           <div className="flex gap-3 items-center py-5 min-w-[120px] max-h-[30px]">
             <FaSubway size={25} />
