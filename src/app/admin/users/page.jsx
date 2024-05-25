950ad81e168f1fb282f30652d56c8cd0b7a16399
Abenezer <abenezerberhanu40@gmail.com>
2024-05-25 14:31:55 +0300

    ADD:real data to main users admin page

--- src/app/admin/users/page.jsx
+++ src/app/admin/users/page.jsx
@@ -1,11 +1,14 @@
-import AdminSmallUsersTable from '@/components/uiComponents/AdminSmallUsersTable'
+import AdminAllUsersTable from '@/components/uiComponents/AdminAllUsersTable'
+import { getAllUsers } from '@/lib/actions/users';
 import React from 'react'
 
-function page() {
+async function page() {
+  const usersRes = await getAllUsers();
+  const users = usersRes ? JSON.parse(usersRes) : [];
   return (
     <div className='flex flex-col gap-3'>
       <h1 className='text-2xl font-bold'>All Users</h1>
-      <AdminSmallUsersTable />
+      <AdminAllUsersTable users={users} />
     </div>
   )
 }
---
+++ src/components/uiComponents/AdminAllUsersTable.jsx
@@ -0,0 +1,47 @@
+"use client";
+import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
+import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "../ui/button";
+
+export default function AdminAllUsersTable({ users }) {
+  return (
+    <Table>
+      <Thead>
+        <Tr className="bg-primary h-12">
+          <Th className="text-white text-start">Profile</Th>
+          <Th className="text-white text-start">Name</Th>
+          <Th className="text-white text-start">Email</Th>
+          <Th className="text-white text-start">Role</Th>
+          <Th className="text-white text-start">Action</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {users?.slice(0, 15).map((user, idx) => (
+          <Tr key={idx} className={`${idx % 2 === 0 && "bg-slate-100"}`}>
+            <Td className="text-sm font-semibold py-3">
+              <Avatar className="size-6 border">
+                <AvatarImage src={user?.photo} />
+                <AvatarFallback></AvatarFallback>
+              </Avatar>
+            </Td>
+            <Td className="text-sm font-semibold py-1">{user.name}</Td>
+            <Td className="text-sm font-semibold py-1">{user.email}</Td>
+            <Td className="text-sm font-semibold py-1">{user.role}</Td>
+            <Td className="text-sm font-semibold py-1">
+              <form action="">
+                <input type="hidden" name="id" value={user?._id} />
+                <Button
+                  variant="ghost"
+                  className="bg-transparent hover:bg-transparent p-0 text-red-500 hover:underline hover:text-red-600"
+                >
+                  Delete
+                </Button>
+              </form>
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
+}
