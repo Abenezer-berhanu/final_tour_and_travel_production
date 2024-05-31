4e4d9763896d658842c6b6383641a8e7c2917124
Abenezer <abenezerberhanu40@gmail.com>
2024-06-01 01:07:48 +0300

    ADD:rating filter

--- src/components/uiComponents/AdminUsersFilter.jsx
+++ src/components/uiComponents/AdminUsersFilter.jsx
@@ -1,5 +1,10 @@
 "use client";
-import { useRouter, useSearchParams, usePathname } from "next/navigation";
+import {
+  useRouter,
+  useSearchParams,
+  usePathname,
+  redirect,
+} from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function AdminUsersFilter() {
@@ -8,17 +13,13 @@ export default function AdminUsersFilter() {
   const pathname = usePathname();
   const params = new URLSearchParams(searchParams);
 
-  useEffect(() => {
-    // const difficultyParam = searchParams.get("difficulty");
-  }, [searchParams]);
+  const setParam = (active, value) => {
+    params.set(active, value);
+    replace(`${pathname}?${params}`);
+  };
 
   const handleChange = (e) => {
-    console.log(e.target.name + e.target.checked); // easyfalse || easytrue
-    //if the coming name is their in array of difficulty query and if the checked is false delete it
-    //if the coming name is not in array of difficulty query and if the checked is true add it and push us again
     if (e.target.checked) {
-      //check if diff query exist and if it exists get all change them to array and find that and if the name in it
-      //if it exist don't do anything else if you don't find the name in param then add it to array and change it to the string and push us
       const isDiffExist = searchParams.has("diff");
       if (isDiffExist) {
         const existPrams = searchParams.get("diff")?.split(",");
@@ -28,12 +29,10 @@ export default function AdminUsersFilter() {
         }
         existPrams?.push(e.target.name.toString());
         const stringFromParam = existPrams?.toString().split(",").join(",");
-        params.set("diff", stringFromParam);
-        replace(`${pathname}?${params}`);
+        setParam("diff", stringFromParam);
       } else {
         //create new diff query with the values from
-        params.set("diff", e.target.name);
-        replace(`${pathname}?${params}`);
+        setParam("diff", e.target.name);
       }
     } else {
       const existPrams = searchParams.get("diff")?.split(",");
@@ -44,8 +43,27 @@ export default function AdminUsersFilter() {
       }
       const existQuery = existPrams?.filter((p) => p != e.target.name);
       const stringFromParam = existQuery?.toString().split(",").join(",");
-      params.set("diff", stringFromParam);
-      replace(`${pathname}?${params}`);
+      setParam("diff", stringFromParam);
+    }
+  };
+
+  const handleRatingChange = (e) => {
+    console.log(e.target.value == 0); // min2 max3
+    if (e.target.name == "min") {
+      if (e.target.value == 0) {
+        console.log("working");
+        params.delete("min");
+        replace(`${pathname}?${params}`);
+        return;
+      }
+      setParam("min", String(e.target.value));
+    } else if (e.target.name == "max") {
+      if (e.target.value == 0) {
+        params.delete("max");
+        replace(`${pathname}?${params}`);
+        return;
+      }
+      setParam("max", String(e.target.value));
     }
   };
 
@@ -108,7 +126,7 @@ export default function AdminUsersFilter() {
       <div className="relative">
         <details className="group [&_summary::-webkit-details-marker]:hidden">
           <summary className="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
-            <span className="text-sm font-medium"> Price </span>
+            <span className="text-sm font-medium"> Rating </span>
 
             <span className="transition group-open:-rotate-180">
               <svg
@@ -129,19 +147,11 @@ export default function AdminUsersFilter() {
           </summary>
 
           <div className="z-50 group-open:absolute group-open:start-0 group-open:top-auto group-open:mt-2">
-            <div className="w-96 rounded border border-gray-200 bg-white">
+            <div className="w-fit rounded border border-gray-200 bg-white">
               <header className="flex items-center justify-between p-4">
                 <span className="text-sm text-gray-700">
-                  {" "}
-                  The highest price is $600{" "}
+                  The highest rating is 5
                 </span>
-
-                <button
-                  type="button"
-                  className="text-sm text-gray-900 underline underline-offset-4"
-                >
-                  Reset
-                </button>
               </header>
 
               <div className="border-t border-gray-200 p-4">
@@ -150,28 +160,37 @@ export default function AdminUsersFilter() {
                     htmlFor="FilterPriceFrom"
                     className="flex items-center gap-2"
                   >
-                    <span className="text-sm text-gray-600">$</span>
+                    <span className="text-sm text-gray-600">⭐</span>
 
-                    <input
-                      type="number"
-                      id="FilterPriceFrom"
-                      placeholder="From"
-                      className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                    />
+                    <select
+                      name="min"
+                      className="py-1 outline-none border border-slate-400 rounded-md w-16"
+                      onChange={handleRatingChange}
+                    >
+                      {Array.from({ length: 4 }).map((_, idx) => (
+                        <option value={idx} key={idx + 1}>
+                          {idx}
+                        </option>
+                      ))}
+                    </select>
                   </label>
-
+                  -
                   <label
                     htmlFor="FilterPriceTo"
                     className="flex items-center gap-2"
                   >
-                    <span className="text-sm text-gray-600">$</span>
-
-                    <input
-                      type="number"
-                      id="FilterPriceTo"
-                      placeholder="To"
-                      className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                    />
+                    <span className="text-sm text-gray-600">⭐</span>
+                    <select
+                      name="max"
+                      className="py-1 outline-none border border-slate-400 rounded-md w-16"
+                      onChange={handleRatingChange}
+                    >
+                      {Array.from({ length: 5 }).map((_, idx) => (
+                        <option value={idx} key={idx + 1}>
+                          {idx}
+                        </option>
+                      ))}
+                    </select>
                   </label>
                 </div>
               </div>
