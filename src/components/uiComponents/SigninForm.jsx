0640a1c3792369c26526918ff51d52151c8437e3
Abenezer <abenezerberhanu40@gmail.com>
2024-06-01 09:31:32 +0300

    FIX: redirect after signin

--- src/components/uiComponents/SigninForm.jsx
+++ src/components/uiComponents/SigninForm.jsx
@@ -6,17 +6,16 @@ import { useFormState, useFormStatus } from "react-dom";
 import { loginUser } from "@/lib/actions/users";
 import Spinner from "./Spinner";
 import { IoEyeOff, IoEye } from "react-icons/io5";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import { toast } from "react-toastify";
 
 export default function SigninForm() {
   const [showPassword, setShowPassword] = useState(false);
-  const { push } = useRouter();
   const [state, formAction] = useFormState(loginUser, null);
   useEffect(() => {
     if (state?.success) {
       toast.success(state.success);
-      push("/");
+      redirect("/");
     } else {
       toast.error(state?.error);
     }
