e5426c6624f9ceff5f4186ccbe14479797a98136
Abenezer <abenezerberhanu40@gmail.com>
2024-06-08 16:49:17 +0300

    stripe payment verifcation

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -56,7 +56,6 @@ export async function POST(req, res) {
 
       const bookItSelf = await bookModel
         .findById(bookedTourId)
-        .populate("user")
         .populate("tour");
       console.log("bookItSelf", bookItSelf);
 
--- src/lib/actions/book.js
+++ src/lib/actions/book.js
@@ -35,6 +35,7 @@ export const findBookById = async (id) => {
       .findById(id)
       .populate("user")
       .populate("tour")
+      .lean();
     return tour;
   } catch (error) {
     console.log(error);
