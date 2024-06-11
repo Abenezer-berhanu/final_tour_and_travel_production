4ed381b5e0a57b33064ee2618ef740d42c07937c
Abenezer <abenezerberhanu40@gmail.com>
2024-06-11 13:02:28 +0300

    REFACTOR: footer refactor

--- src/components/uiComponents/Footer.jsx
+++ src/components/uiComponents/Footer.jsx
@@ -1,31 +1,11 @@
 import NavbarLogo from "./NavbarLogo";
+import Link from "next/link";
 
 function Footer() {
+  const date = new Date().getFullYear();
   return (
     <footer className="bg-gray-100 mt-5">
       <div className="relative mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 lg:pt-24">
-        <div className="absolute end-4 top-4 sm:end-6 sm:top-6 lg:end-8 lg:top-8">
-          <a
-            className="inline-block rounded-full bg-teal-600 p-2 text-white shadow transition hover:bg-teal-500 sm:p-3 lg:p-4"
-            href="#MainContent"
-          >
-            <span className="sr-only">Back to top</span>
-
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>
-        </div>
-
         <div className="lg:flex lg:items-end lg:justify-between">
           <div>
             <div className="flex justify-center text-teal-600 lg:justify-start">
@@ -40,45 +20,29 @@ function Footer() {
 
           <ul className="mt-12 flex flex-wrap justify-center gap-6 md:gap-8 lg:mt-0 lg:justify-end lg:gap-12">
             <li>
-              <a
+              <Link
                 className="text-gray-700 transition hover:text-gray-700/75"
-                href="#"
+                href={"https://www.yamatoursethiopia.com/about-us.html"}
+                target="_blank"
               >
                 About
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 className="text-gray-700 transition hover:text-gray-700/75"
-                href="#"
-              >
-                Services
-              </a>
-            </li>
-
-            <li>
-              <a
-                className="text-gray-700 transition hover:text-gray-700/75"
-                href="#"
-              >
-                Projects
-              </a>
-            </li>
-
-            <li>
-              <a
-                className="text-gray-700 transition hover:text-gray-700/75"
-                href="#"
+                href={"https://www.yamatoursethiopia.com/blog.html"}
+                target="_blank"
               >
                 Blog
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
 
         <p className="mt-12 text-center text-sm text-gray-500 lg:text-right">
-          Copyright &copy; 2022. All rights reserved.
+          Copyright &copy; {date}. All rights reserved.
         </p>
       </div>
     </footer>
