28fc3716fce14cf11934d11b0c6ea44fad5746d8
Abenezer <abenezerberhanu40@gmail.com>
2024-06-13 00:41:52 +0300

    remove my tours if not user

--- src/components/uiComponents/NavbarUser.jsx
+++ src/components/uiComponents/NavbarUser.jsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { findUserById, signUserOut } from "@/lib/actions/users";
 import Link from "next/link";
-import { Badge } from "@/components/ui/badge"
+import { Badge } from "@/components/ui/badge";
 
 async function NavbarUser() {
   const userRes = await findUserById();
@@ -27,18 +27,23 @@ async function NavbarUser() {
             </Avatar>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuLabel className="flex gap-2">{user?.name}<Badge variant="secondary">{user?.role}</Badge></DropdownMenuLabel>
+            <DropdownMenuLabel className="flex gap-2">
+              {user?.name}
+              <Badge variant="secondary">{user?.role}</Badge>
+            </DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
               <Link href={"/user/profile"} className="h-full w-full">
                 Profile
               </Link>
             </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Link href={"/user/tours"} className="h-full w-full">
-                My tours
-              </Link>
-            </DropdownMenuItem>
+            {user?.role.toLowerCase() == "user" && (
+              <DropdownMenuItem>
+                <Link href={"/user/tours"} className="h-full w-full">
+                  My tours
+                </Link>
+              </DropdownMenuItem>
+            )}
             {user?.role.toLowerCase() != "user" && (
               <DropdownMenuItem>
                 <Link href={"/admin/dashboard"} className="h-full w-full">
